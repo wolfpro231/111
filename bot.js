c150a725a6fec9616a8d68ab055657659066a61d
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-05-20 22:20:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = 'T'
+var adminprefix = '!'
 
 
 const developers = ["483659658461052928"]
